3fb8b7b8c41e455517e60fd4bb8108abf2c9c99a
fceja <cisco_ceja@icloud.com>
2023-03-03 13:43:54 -0800

    add base api

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	store, err := NewpostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	server := NewAPIServer(":3000", store)
+	server.Run()
+}
